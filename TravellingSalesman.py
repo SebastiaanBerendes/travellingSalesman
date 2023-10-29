33bb37f081d1c90f5e0ea2a2098078d05283e24b
Sebastiaan Berendes <sebastiaan.berendes@student.kuleuven.be>
2023-10-29 20:00:30 +0100

    mutation written

--- TravellingSalesman.py
+++ TravellingSalesman.py
@@ -1,4 +1,4 @@
-import random
+import random as rd
 import pandas as pd
 import numpy as np
 
@@ -10,16 +10,24 @@ class TravellingSalesmanProblem:
 class Salesman:
     def __init__(self, tsp):
         cities = len(tsp.dm)  # amount of cities
-        self.path = random.sample(range(1, cities+1), cities)  # generates a random array of the cities
+        self.path = rd.sample(range(1, cities+1), cities)  # generates a random array of the cities
         # calculate distance of path
         d_array = [tsp.dm[self.path[i]-1, self.path[i+1]-1] for i in range(0, len(self.path)-1)] + [tsp.dm[self.path[-1]-1, self.path[1]-1]]
         self.d = sum(d_array)
 
+def mutate(sm, alpha):
+    if rd.random() < alpha:
+        end = rd.randint(0, len(sm.path) - 1)
+        size = rd.randint(2, len(sm.path))
+        sel_cities = [sm.path[i] for i in range(end, end - size, -1)]
+        for i in range(len(sel_cities)):
+            sm.path[end - size + 1 + i] = sel_cities[i]
+
+
+
 
 tsp = TravellingSalesmanProblem()
-index = 0
-while index < 1000:
-    s = Salesman(tsp)
-    if s.d != np.inf:
-        print(s.d)
-    index += 1
+sm = Salesman(tsp)
+print(sm.path)
+mutate(sm, 1)
+print(sm.path)
